0b2df066cf2c2882ee77ca04420c04cb596efa97
Taaniel Jakobson <taanieljak@users.noreply.github.com>
2019-09-10 11:09:59 +0300

    Esimene üleslaadimine

---
+++ Tund_2/testpage.php
@@ -0,0 +1,24 @@
+<?php
+  $userName = "Taaniel Jakobson";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+  echo $userName;
+  ?>
+  programmeerib veebi</title>
+  
+</head>
+<body>
+<?php
+  echo "<h1>" .$userName .", veebiprogrammeerimine</h1>";
+?>
+  <h1>Taaniel Jakobson, veebiprogrammeerimine</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+
+
+</body>
+</html>
